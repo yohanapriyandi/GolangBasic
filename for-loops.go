63d58499d42ea237a29b0e87481798c2a6788e3a
Devbeard <cepoangkuningan@gmail.com>
2024-03-04 17:21:18 +0700

    add comment for loops using statement

--- for-loops.go
+++ for-loops.go
@@ -4,19 +4,20 @@ import "fmt"
 
 func main() {
 	// for loops meggunakan statement
+	fmt.Println("=================for loops using statement====================")
 	for counter := 1; counter <= 10; counter++ {
 		fmt.Println("perulangan ke", counter)
 	}
-
+	fmt.Println("=======================================")
 	slice := []string{"yohan", "Nabilah", "fathar", "tsabita"}
 	for i := 0; i < len(slice); i++ {
 		fmt.Println(slice[i])
 	}
-
+	fmt.Println("=======================================")
 	for i, v := range slice {
 		fmt.Println("Index", i, "=", v)
 	}
-
+	fmt.Println("=======================================")
 	person := make(map[string]string)
 	person["name"] = "Yohan"
 	person["title"] = "programmer"
